7e43d7ac55d45bae6ed15100d4468e5bd4ff0c2f
jtkuhn <jtkuhn@foxguardsolutions.com>
2015-04-27 08:28:13 -0400

    Test primefactor commit

---
+++ PrimeFactorKata/PrimeFactors.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFactorKata
+{
+    public class PrimeFactors
+    {
+        private int number;
+        private int currentPrime = 3;
+
+        List<int> factors = new List<int>();
+
+        public void SetNumber(int num)
+        {
+            number = num;
+        }
+
+        public List<int> GetPrimeFactors()
+        {
+            AddTwos();
+            TestAndAddPrimes();
+            return factors;
+        }
+
+        private void TestAndAddPrimes()
+        {
+            while (number != 1)
+            {
+                if (TestCurrentPrime() == false)
+                {
+                    currentPrime = GetNextPrime();
+                }
+            }
+        }
+
+        private bool TestCurrentPrime()
+        {
+            if (number%currentPrime == 0)
+            {
+                factors.Add(currentPrime);
+                number /= currentPrime;
+                return true;
+            }
+            return false;
+        }   
+
+        //This guy needs some love but oh well
+        private int GetNextPrime()
+        {
+            currentPrime += 2;
+            return currentPrime;
+
+        }
+
+        private void AddTwos()
+        {
+            while (number % 2 == 0)
+            {
+                number /= 2;
+                factors.Add(2);
+            }
+        }
+
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using PrimeFactorKata;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        private PrimeFactors numberToFactor;
+        List<int> list;
+
+        [SetUp]
+        public void Init()
+        {
+            list = new List<int>();
+            numberToFactor =  new PrimeFactors();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            numberToFactor.SetNumber(1);
+            Assert.IsEmpty(numberToFactor.GetPrimeFactors());
+        }
+
+        [Test]
+        public void Test2()
+        {
+            list.Add(2);
+            numberToFactor.SetNumber(2);
+            Assert.AreEqual(list, numberToFactor.GetPrimeFactors());
+        }
+
+        [Test]
+        public void Test4()
+        {
+            list.Add(2);
+            list.Add(2);
+            numberToFactor.SetNumber(4);
+            Assert.AreEqual(list, numberToFactor.GetPrimeFactors());
+        }
+
+        [Test]
+        public void Test3()
+        {
+            list.Add(3);
+            numberToFactor.SetNumber(3);
+            Assert.AreEqual(list, numberToFactor.GetPrimeFactors());
+        }
+
+        [Test]
+        public void Test6()
+        {
+            list.Add(2);
+            list.Add(3);
+            numberToFactor.SetNumber(6);
+            Assert.AreEqual(list, numberToFactor.GetPrimeFactors());
+        }
+
+        [Test]
+        public void Test10()
+        {
+            list.Add(2);
+            list.Add(5);
+            numberToFactor.SetNumber(10);
+            Assert.AreEqual(list, numberToFactor.GetPrimeFactors());
+        }
+
+        [Test]
+        public void Test180()
+        {
+            list.Add(2);
+            list.Add(2);
+            list.Add(3);
+            list.Add(3);
+            list.Add(5);
+            numberToFactor.SetNumber(180);
+            Assert.AreEqual(list, numberToFactor.GetPrimeFactors());
+        }
+
+        [Test]
+        public void Test368()
+        {
+            list.Add(2);
+            list.Add(2); 
+            list.Add(2);
+            list.Add(2);
+            list.Add(23);
+            numberToFactor.SetNumber(368);
+            Assert.AreEqual(list, numberToFactor.GetPrimeFactors());
+        }
+
+        [TestCase(20, new[] {2, 2, 5})]
+        [TestCase(25, new[] {5, 5})]
+        public void Test20(int number, IEnumerable<int> expectedPrimes)
+        {
+            numberToFactor.SetNumber(number);
+            Assert.AreEqual(expectedPrimes, numberToFactor.GetPrimeFactors());
+        }
+    }
+}
